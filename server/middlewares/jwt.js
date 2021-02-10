bea69c48c96b1c3ec93d22c0604ce5ad2090061a
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-02-10 03:13:19 +0200

    finished user controller

--- server/middlewares/jwt.js
+++ server/middlewares/jwt.js
@@ -1,5 +1,8 @@
 import jwt from 'jsonwebtoken';
+// models
+import UserModel from '../models/User.js';
 
+const SECRET_KEY = 'some-secret-key';
 export const decode = (req, res, next) => {}
 
 export const encode = async (req, res, next) => {}
