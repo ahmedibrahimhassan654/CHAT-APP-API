11d5e8a4bdc1f008310a912b4480636d21d4b9ea
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-02-10 04:57:24 +0200

    encoded and decoded jwt

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,6 +7,14 @@ import { encode } from '../middlewares/jwt.js';
 const router = express.Router();
 
 router
-  .post('/login/:userId', encode, (req, res, next) => { });
+  .post('/login/:userId', encode, (req, res, next) => { 
+
+    return res
+      .status(200)
+      .json({
+        success: true,
+        authorization: req.authToken,
+      });
+  });
 
 export default router;
