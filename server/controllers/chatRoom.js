b91dee40d73a4e4c361aa820d3250ebe743f82d7
Mohamed Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-02-13 14:53:14 +0200

    create message in chatoom

--- server/controllers/chatRoom.js
+++ server/controllers/chatRoom.js
@@ -1,9 +1,6 @@
 import makeValidation from '@withvoid/make-validation';
 import ChatRoomModel, { CHAT_ROOM_TYPES } from '../models/ChatRoom.js';
-// const CHAT_ROOM_TYPES = {
-//   CONSUMER_TO_CONSUMER: "consumer-to-consumer",
-//   CONSUMER_TO_SUPPORT: "consumer-to-support",
-// };
+import ChatMessageModel from  "../models/ChatMessage.js"
 
 export default {
     initiate: async (req, res) => {
@@ -32,7 +29,29 @@ export default {
         return res.status(500).json({ success: false, error: error })
       }
      },
-    postMessage: async (req, res) => { },
+    postMessage: async (req, res) => {
+      try {
+        const { roomId } = req.params;
+        const validation = makeValidation(types => ({
+          payload: req.body,
+          checks: {
+            messageText: { type: types.string },
+          }
+        }));
+        if (!validation.success) return res.status(400).json({ ...validation });
+    
+        const messagePayload = {
+          messageText: req.body.messageText,
+        };
+        const currentLoggedUser = req.userId;
+        const post = await ChatMessageModel.createPostInChatRoom(roomId, messagePayload, currentLoggedUser);
+        global.io.sockets.in(roomId).emit('new message', { message: post });
+        return res.status(200).json({ success: true, post });
+      } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, error: error })
+      }
+     },
     getRecentConversation: async (req, res) => { },
     getConversationByRoomId: async (req, res) => { },
     markConversationReadByRoomId: async (req, res) => { },
